cef0e444816eb6478a4b30dfe7a6963b8d81c034
Michael Clark <mclark@ipsolonresearch.com>
2022-03-17 16:28:42 -0400

    improved plot labels

--- host/matlab/request_samples.m
+++ host/matlab/request_samples.m
@@ -16,11 +16,19 @@ cwgen_ampl_scale = 0;       % CWGEN amplitude scale (x * 2^-scale)
 
 iq = cerb_request_samples(ip, nsamps, cwgen_freq_mhz, cwgen_ampl_scale);
 
+%%
 figure;
 subplot(2,1,1); plot(real(iq));
+title('Real Waveform');
+xlabel('Samples');
+ylabel('Amplitude');
 subplot(2,1,2); plot(imag(iq));
-title('IQ Waveform');
+title('Imag Waveform');
+xlabel('Samples');
+ylabel('Amplitude');
+sgtitle('Complex Waveform') 
 
+%%
 figure;
 N   = length(iq);
 Nyq = 1/2 * fs_mhz;
@@ -30,11 +38,14 @@ G   = fftshift(abs(fft(iq))/N);
 P   = 10*log10(G.^2 + 1e-16);
 plot(f,P);
 title('Power Spectrum');
+xlabel('Frequecy (MHz)');
+ylabel('Power (dB)');
 
-% look for discontinuities
+%% 
+% check for discontinuities
 figure, plot(diff(unwrap(angle(iq))));
 title('Change in Phase');
-
-
+xlabel('Samples');
+ylabel('Amplitude');
 
 
